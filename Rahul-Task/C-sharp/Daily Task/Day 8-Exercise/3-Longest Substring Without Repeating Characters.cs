3a99b164996ef439b1b01dd0b4011a6b3a4859f0
neoinfoway707 <neoinfoway707@gmail.com>
2026-05-29 16:29:43 +0530

    Leetcode Task 3 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/3-Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    internal class _3_Longest_Substring_Without_Repeating_Characters
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                string str = "asqasdsd";
+                Console.WriteLine($"String {str} in longest substring length is {LengthOfLongestSubstring(str)}.");
+
+            }
+            public static int LengthOfLongestSubstring(string s)
+            {
+                string substring = s;
+                HashSet<char> map = new HashSet<char>();
+
+                int max = 0;
+                int start = 0;
+                for (int i = 0; i < substring.Length; i++)
+                {
+                    while (map.Contains(substring[i]))
+                    {
+                        map.Remove(substring[start]);
+                        start = start + 1;
+                    }
+                    map.Add(substring[i]);
+                    max = Math.Max(max, i - start + 1);
+                }
+                return max;
+            }
+        }
+    }
+}
