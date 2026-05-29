e538d5dc3af0f14efce4bfe618a31a8e19118481
neoinfoway707 <neoinfoway707@gmail.com>
2026-05-29 16:27:48 +0530

    Leetcode Task 1 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/1-Two Sum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    internal class _1_Two_Sum
+    {
+        public class Solution
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("======= Two Sum =======");
+                int[] arr = { 12, 5, 13, 5, 1, 20 };
+                int[] res = TwoSum(arr, 13);
+                if (res != null)
+                {
+                    Console.WriteLine("Two Sum: " + res[0] + ", " + res[1]);
+                }
+                else
+                {
+                    Console.WriteLine("Not Found.");
+                }
+            }
+            public static int[] TwoSum(int[] nums, int target)
+            {
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            return new int[] { i, j };
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
