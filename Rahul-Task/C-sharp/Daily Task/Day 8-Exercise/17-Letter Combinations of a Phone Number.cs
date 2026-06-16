be5d6aac69dd2d2e3e8a34071beebfa85bc32258
neoinfoway707 <neoinfoway707@gmail.com>
2026-06-16 11:51:12 +0530

    Leetcode Task 17 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/17-Letter Combinations of a Phone Number.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _17_Letter_Combinations_of_a_Phone_Number
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                string digits = "23";
+                var result = LetterCombinations(digits);
+                Console.WriteLine(string.Join(",", result));
+            }
+            public static IList<string> LetterCombinations(string digits)
+            {
+                string[] phone = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+                List<string> result = new List<string>();
+
+                void Backtrack(string current, int index)
+                {
+                    if (index == digits.Length)
+                    {
+                        result.Add(current);
+                        return;
+                    }
+                    string letters = phone[digits[index] - '0'];
+                    foreach (char letter in letters)
+                    {
+                        Backtrack(current + letter, index + 1);
+                    }
+                }
+                Backtrack("", 0);
+                return result;
+            }
+        }
+    }
+}
