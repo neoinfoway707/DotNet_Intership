d69764b0ccfc7d3bc32664cf97266c74793dd6ab
neoinfoway707 <neoinfoway707@gmail.com>
2026-06-29 10:57:16 +0530

    Leetcode Task 26 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/26-Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _26_Remove_Duplicates_from_Sorted_Array
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int[] nums = { 1, 2 };
+                var result = RemoveDuplicates(nums);
+                Console.WriteLine(result);
+            }
+        }
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int slow = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[slow])
+                    continue;
+                else
+                {
+                    slow++;
+                    nums[slow] = nums[i];
+                }
+            }
+            return slow + 1;
+        }
+    }
+}
