00da796e0a3075d3908e3b4c0745d848e1aeb6e4
neoinfoway707 <neoinfoway707@gmail.com>
2026-06-30 10:31:03 +0530

    Leetcode Task 27 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/27-Remove Element.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _27_Remove_Element
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int[] nums = { 1, 2, 1, 4, 1, 3, 1 };
+                var result = RemoveElement(nums, 1);
+                Console.WriteLine(result);
+            }
+            public static int RemoveElement(int[] nums, int val)
+            {
+                int count = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == val)
+                        continue;
+                    else
+                    {
+                        nums[count] = nums[i];
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
