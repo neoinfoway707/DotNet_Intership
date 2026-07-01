7eda2a01e54ee4d0e1355e164ebdf5379b23eec7
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-01 10:38:58 +0530

    Leetcode Task 28 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/28-Find the Index of the First Occurrence in a String.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                string needle = "sad";
+                string haystack = "sabutsad";
+                var result = StrStr(haystack, needle);
+                Console.WriteLine(result);
+            }
+            public static int StrStr(string haystack, string needle)
+            {
+                for (int i = 0; i <= haystack.Length - needle.Length; i++)
+                {
+                    if (haystack.Substring(i, needle.Length) == needle)
+                        return i;
+                }
+                return -1;
+            }
+        }
+    }
+}
