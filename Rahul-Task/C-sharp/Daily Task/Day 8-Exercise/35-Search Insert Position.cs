dcf2e8d19366715101d3f5af89b59882f7a17f39
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-10 15:21:48 +0530

    Leetcode Task 35 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/35-Search Insert Position.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _35_Search_Insert_Position
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                var result = SearchInsert(new int[] { 1, 3, 5, 6 }, 2);
+                Console.WriteLine(result);
+            }
+            public static int SearchInsert(int[] nums, int target)
+            {
+                int low = 0;
+                int high = nums.Length - 1;
+                while (low <= high)
+                {
+                    int mid = (low + high) / 2;
+                    if (nums[mid] == target) return mid;
+                    else if (nums[mid] > target)
+                        high = mid - 1;
+                    else
+                        low = mid + 1;
+                }
+                return low;
+            }
+        }
+    }
+}
