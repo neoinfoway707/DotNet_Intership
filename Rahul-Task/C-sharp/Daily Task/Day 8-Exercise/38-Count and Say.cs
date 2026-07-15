f7a4ae36741769615121e5d1ebf1813d1d909536
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-15 11:06:24 +0530

    Leetcode Task 38 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/38-Count and Say.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _38_Count_and_Say
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                var result = CountAndSay(5);
+                Console.WriteLine(result);
+            }
+            public static string CountAndSay(int n)
+            {
+                if (n == 1) return "1";
+                var prev = CountAndSay(n - 1);
+
+                StringBuilder result = new StringBuilder();
+                int count = 1;
+
+                for (int i = 1; i < prev.Length; i++)
+                {
+                    if (prev[i] == prev[i - 1])
+                        count++;
+                    else
+                    {
+                        result.Append(count);
+                        result.Append(prev[i - 1]);
+                        count = 1;
+                    }
+                }
+                result.Append(count);
+                result.Append(prev[prev.Length - 1]);
+
+                return result.ToString();
+            }
+          /*public static string CountAndSay(int n)
+            {
+                if (n == 1) return "1";
+                var prev = CountAndSay(n - 1);
+                string result = "";
+                int count = 1;
+
+                for (int i = 1; i < prev.Length; i++)
+                {
+                    if (prev[i] == prev[i - 1])
+                        count++;
+                    else
+                    {
+                        result += count + "" + prev[i - 1];
+                        count = 1;
+                    }
+                }
+                result += count + "" + prev[prev.Length - 1];
+
+                return result;
+            }*/
+        }
+    }
+}
