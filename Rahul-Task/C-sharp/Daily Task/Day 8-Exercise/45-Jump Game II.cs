81f69db93d2bea1789fa22f6124e00a4bc50799f
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-24 11:17:38 +0530

    Leetcode Task 45 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/45-Jump Game II.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _45_Jump_Game_II
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                var result = Jump(new int[] { 2, 3, 1, 1, 4 });
+                Console.WriteLine(result);
+            }
+            public static int Jump(int[] nums)
+            {
+                int currentEnd = 0, farthest = 0, jumps = 0;
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    farthest = Math.Max(farthest, i + nums[i]);
+                    if (i == currentEnd)
+                    {
+                        jumps++;
+                        currentEnd = farthest;
+                    }
+                }
+                return jumps;
+            }
+        }
+    }
+}
