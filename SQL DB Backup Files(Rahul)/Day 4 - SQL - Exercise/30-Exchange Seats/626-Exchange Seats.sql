5250a9b9fd2c7c893e95e362ef40d85684fa5f8b
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-28 11:34:55 +0530

    Leetcode SQL Task 30 Complete

---
+++ SQL DB Backup Files(Rahul)/Day 4 - SQL - Exercise/30-Exchange Seats/626-Exchange Seats.sql
@@ -0,0 +1,10 @@
+SELECT * FROM Seat;
+
+SELECT 
+	s1.id ,
+	COALESCE(s2.student, s1.student) AS Student
+FROM Seat s1 
+LEFT JOIN Seat s2 
+	ON (s1.id % 2 = 1 AND s2.id = s1.id + 1) 
+	OR (s1.id % 2 = 0 AND s2.id = s1.id - 1)
+ORDER BY s1.id;
