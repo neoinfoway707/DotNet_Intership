a8dfcf91d7204a1e1296bf217190a8fc22d1b4a9
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-29 12:36:58 +0530

    Leetcode Task 48 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/48-Rotate Image.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _48_Rotate_Image
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                var matrix = new int[][]
+                {
+                    new int[]{1,2,3},
+                    new int[]{4,5,6},
+                    new int[]{7,8,9 }
+                };
+
+                Rotate(matrix);
+
+                foreach (var row in matrix)
+                {
+                    Console.WriteLine(string.Join(" ", row));
+                }
+            }
+            public static void Rotate(int[][] matrix)
+            {
+                var n = matrix.Length;
+                for (int layer = 0; layer < n / 2; layer++)
+                {
+                    int left = layer;
+                    int right = n - 1 - layer;
+                    for (int i = left; i < right; i++)
+                    {
+                        int offset = i - left;
+
+                        int top = matrix[left][i];
+                        matrix[left][i] = matrix[right - offset][left];
+                        matrix[right - offset][left] = matrix[right][right - offset];
+                        matrix[right][right - offset] = matrix[i][right];
+                        matrix[i][right] = top;
+                    }
+                }
+            }
+        }
+    }
+}
