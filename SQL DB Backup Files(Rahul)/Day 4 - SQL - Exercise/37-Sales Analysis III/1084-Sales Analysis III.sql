4db584951bdded585935358426529fc364c505a6
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-31 12:47:50 +0530

    Leetcode SQL Task 37 Complete

---
+++ SQL DB Backup Files(Rahul)/Day 4 - SQL - Exercise/37-Sales Analysis III/1084-Sales Analysis III.sql
@@ -0,0 +1,22 @@
+SELECT * FROM Product;
+SELECT * FROM Sales;
+
+SELECT product_id, product_name 
+FROM Product
+WHERE product_id NOT IN (
+    SELECT product_id 
+    FROM Sales
+    WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
+)
+AND product_id IN (
+    SELECT product_id 
+    FROM Sales
+    WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+);
+
+SELECT p.product_id, p.product_name
+FROM Product AS p
+JOIN Sales AS s
+ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31';
