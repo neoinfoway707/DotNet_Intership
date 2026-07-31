588651967bdf26f87227e56b40de120734e4fe27
neoinfoway707 <neoinfoway707@gmail.com>
2026-07-31 12:47:22 +0530

    Leetcode Task 50 Complete

---
+++ Rahul-Task/C-sharp/Daily Task/Day 8-Exercise/50-Pow(x, n).cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace C_sharp.Daily_Task.Day_8_Exercise
+{
+    public class _50_Pow_x__n_
+    {
+        public class Solution
+        {
+            public static void Main(string[] args)
+            {
+                var result = MyPow(2, -2);
+                Console.WriteLine(result);
+            }
+            public static double MyPow(double x, int n)
+            {
+                long power = n;
+                if( power<0)
+                {
+                    x = 1 / x;
+                    power = -power;
+                }
+                double result = 1;
+                double currentPower = x;
+                while(power > 0)
+                {
+                    if(power%2 == 0)
+                    {
+                        currentPower *= currentPower;
+                        power = power / 2;
+                    }
+                    else
+                    {
+                        result = result * currentPower;
+                        currentPower *= currentPower;
+                        power = power / 2;
+                    }
+                }
+                return result;
+            }
+
+            //Using Recursion
+           /* public static double MyPow(double x, int n)
+            {
+                long num = n;
+                if (n < 0)
+                    return 1 / Pow(x, -num);
+
+                return Pow(x, num);
+            }
+            private static double Pow(double x, long n)
+            {
+                if (n == 0) return 1;
+                if (n % 2 == 0)
+                {
+                    var half = Pow(x, n / 2);
+                    return half * half;
+                }
+                else
+                {
+                    return Pow(x, n - 1) * x;
+                }
+            }*/
+        }
+    }
+}
