dc5a328e83c66823e362d1d8a6c68c3c5ec52fa9
neoinfoway707 <neoinfoway707@gmail.com>
2026-08-05 12:06:15 +0530

    Leetcode SQL Task 50 Complete

---
+++ SQL DB Backup Files(Rahul)/Day 4 - SQL - Exercise/50-List the Products Ordered in a Period/1327-List the Products Ordered in a Period.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Products;
+SELECT * FROM Orders;
+
+SELECT p.product_name, SUM(o.unit) AS unit FROM Products p
+JOIN Orders o ON o.product_id = p.product_id
+WHERE YEAR(o.order_date) = 2020 AND MONTH(o.order_date) = 2
+GROUP BY p.product_name HAVING SUM(o.unit) >= 100;
