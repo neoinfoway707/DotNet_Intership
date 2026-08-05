4160b1ab06089aba67cb533df9eba7d17d508f5d
neoinfoway707 <neoinfoway707@gmail.com>
2026-08-05 12:05:55 +0530

    Leetcode SQL Task 48 Complete

---
+++ SQL DB Backup Files(Rahul)/Day 4 - SQL - Exercise/48-Students and Examinations/1280-Students and Examinations.sql
@@ -0,0 +1,10 @@
+SELECT * FROM Students;
+SELECT * FROM Subjects;
+SELECT * FROM Examinations;
+
+select stud.student_id, stud.student_name, sub.subject_name, count(exam.subject_name) as attended_exams 
+from Students stud
+cross join Subjects sub
+left join Examinations exam on exam.student_id = stud.student_id and exam.subject_name = sub.subject_name
+group by sub.subject_name,stud.student_id , stud.student_name
+order by stud.student_id, sub.subject_name;
